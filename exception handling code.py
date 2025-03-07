480a297ae0e1027393ea81ab919b111344e4d81b
Asmakter <akterasma.me@gmail.com>
2025-03-07 14:16:32 +0600

    exception handing code

---
+++ exception handling code.py
@@ -0,0 +1,26 @@
+# try:
+#     a = float(input("Enter 1st number: "))
+#     b = float(input("Enter 2nd number: "))
+#
+#     div = a/b
+#     print("Division: ", div)
+#
+#
+# except:
+#     print("Cannot divide by 0")
+# finally:
+#     add = a + b
+#     print("Addition: ", add)
+#     print("Always Execute This Block")
+#empty set
+# s = set()
+# print(s)
+#
+# print(x[0:-2])
+# print(x[0:4])
+#same same but different
+#open and read text file
+# with open('D:/doc 1.txt','r') as f:
+#     print(f.read())
+# s ={1,2,3,4,5,6}
+# print(type(s))
