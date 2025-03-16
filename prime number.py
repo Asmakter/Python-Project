f2510f402a9d8f1d7d1cf6eaff0628b9d7d1e119
Asmakter <akterasma.me@gmail.com>
2025-03-16 11:22:26 +0600

    new files added

--- prime number.py
+++ prime number.py
@@ -1,11 +1,35 @@
 #check prime number
-n = int(input("Enter a Number: "))
-flag = 0
-for i in range(2,n):
-    if n%i == 0:
-        flag = 1
-        break
-if flag == 0:
-    print("prime")
-else:
-    print("Not Prime")
+# n = int(input("Enter a Number: "))
+# flag = 0
+# for i in range(2,n):
+#     if n%i == 0:
+#         flag = 1
+#         break
+# if flag == 0:
+#     print("prime")
+# else:
+#     print("Not Prime")
+# s = input("Enter a number: ")
+# sum = 0
+# for i in s:
+#     sum = sum + int(i)
+# print(sum)
+
+# convert integer to string
+
+# n = int(input("Enter a number: "))
+# sum = 0
+# while n != 0:
+#     r = n%10
+#     n = int(n/10)
+#     sum = sum + r
+# print(sum)
+sum = 0
+n = int(input("Enter a number: "))
+if n<0:
+    n = (-1)*n
+while n != 0:
+    r = n%10
+    n = int(n/10)
+    sum = sum + r
+print(sum)
